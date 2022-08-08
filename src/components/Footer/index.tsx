861e47ca2863890949a0c7d8d557336b3b85b408
Micaías Silva <micaiasilvaop@gmail.com>
2022-08-08 12:35:22 -0300

    Erros corrigidos

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,12 @@ import { Text } from "@/styles/Text";
 import { Footer as FooterWrapper } from "./style";
 import { UserImage } from "@/pages/home/style";
 import { userData } from "@/utils/userData";
-import { FaLinkedinIn, FaInstagram, FaFacebookF } from "react-icons/fa";
+import {
+  FaLinkedinIn,
+  FaInstagram,
+  FaTelegram,
+  FaTelegramPlane,
+} from "react-icons/fa";
 import { Button } from "@/styles/Buttons";
 import { HandEffect } from "../HandEffect";
 
@@ -21,8 +26,7 @@ export const Footer = (): JSX.Element => {
           />
           <Box css={{ marginLeft: "$2" }}>
             <Text type="heading4" color="grey5" css={{ marginBottom: "$2" }}>
-              Thank you!{" "}
-             <HandEffect />
+              Thank you! <HandEffect />
             </Text>
             <Text type="body1" color="grey2">
               Follow me on my social networks and let's talk
@@ -30,13 +34,31 @@ export const Footer = (): JSX.Element => {
           </Box>
         </Flex>
         <Flex>
-          <Button className="instagram" type="circle" as="a" target="_blank" href={`https://instagram.com/${userData.instagramUser}`}>
+          <Button
+            className="instagram"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://instagram.com/${userData.instagramUser}`}
+          >
             <FaInstagram />
           </Button>
-          <Button className="facebook" type="circle" as="a" target="_blank" href={`https://fb.com/${userData.facebookUser}`}>
-            <FaFacebookF />
+          <Button
+            className="facebook"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://t.me/${userData.telegramUser}`}
+          >
+            <FaTelegramPlane />
           </Button>
-          <Button className="linkedin" type="circle" as="a" target="_blank" href={`https://linkedin.com/in/${userData.linkedinUser}`}>
+          <Button
+            className="linkedin"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://linkedin.com/in/${userData.linkedinUser}`}
+          >
             <FaLinkedinIn />
           </Button>
         </Flex>
--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -69,7 +69,7 @@ export const Project = (): JSX.Element => {
           </ProjectStack>
 
           <Text type="body1" color="grey2">
-            {repository.description.substring(0, 129)}
+            {repository.description?.substring(0, 129)}
           </Text>
           <ProjectLinks>
             <ProjectLink target="_blank" href={repository.git_url}>
