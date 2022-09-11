228fc46df568f5cbd4177268598f85d0b2a37f1b
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-11 14:18:04 -0300

    Adicionado: Stack TypeScript

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -4,7 +4,9 @@ import imgJS from "../public/static/img/stack/js.svg";
 import imgNode from "../public/static/img/stack/node.svg";
 import imgStyled from "../public/static/img/stack/styled.svg";
 import imgReact from "../public/static/img/stack/react.svg";
-import { FaVuejs } from "react-icons/fa";
+import imgTS from "../public/static/img/stack/ts.svg";
+import imgPostgres from "../public/static/img/stack/postgresql.svg";
+import { FaDocker } from "react-icons/fa";
 
 export const stackData = [
   {
@@ -16,8 +18,12 @@ export const stackData = [
     img: imgCSS,
   },
   {
-    title: "JS",
+    title: "JavaScript",
     img: imgJS,
+  },
+  {
+    title: "TypeScript",
+    img: imgTS,
   },
   {
     title: "Node JS",
@@ -31,4 +37,12 @@ export const stackData = [
     title: "React",
     img: imgReact,
   },
+  {
+    title: "Docker",
+    img: FaDocker,
+  },
+  // {
+  //   title: "PostgreSQL",
+  //   img: imgPostgres,
+  // },
 ];
